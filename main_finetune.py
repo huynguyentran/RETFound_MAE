cc96385755257f9e3c8ef77ed7dcce0f978492c6
Huy Nguyen <huynguyentranwe@gmail.com>
2025-03-27 19:00:03 -0700

    update

--- main_finetune.py
+++ main_finetune.py
@@ -147,7 +147,7 @@ def get_args_parser():
 
 
 def main(args, criterion=torch.nn.CrossEntropyLoss()):
-    torch.serialization.add_safe_globals([argparse.Namespace])
+
     if args.resume and not args.eval:
         resume = args.resume
         checkpoint = torch.load(args.resume, map_location='cpu')
