af0d4d8b49e5068ad6ee36b9abea065343559dd1
Huy Nguyen <77764356+huynguyentran@users.noreply.github.com>
2025-04-11 13:46:07 -0700

    Update engine_finetune.py
    _Update to make sure it works with the new input

--- engine_finetune.py
+++ engine_finetune.py
@@ -39,7 +39,7 @@ def train_one_epoch(
     if log_writer:
         print(f'log_dir: {log_writer.log_dir}')
     
-    for data_iter_step, (samples, targets) in enumerate(metric_logger.log_every(data_loader, print_freq, f'Epoch: [{epoch}]')):
+    for data_iter_step, (samples, _, targets) in enumerate(metric_logger.log_every(data_loader, print_freq, f'Epoch: [{epoch}]')):
         if data_iter_step % accum_iter == 0:
             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
         
